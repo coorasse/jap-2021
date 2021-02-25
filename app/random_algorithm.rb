045f3245362d9d20cd2811522a9c8826615181c9
Josua Schmid <josua.schmid@renuo.ch>
2021-02-25 21:02:33 +0100

    Go

--- app/random_algorithm.rb
+++ app/random_algorithm.rb
@@ -3,9 +3,24 @@ class RandomAlgorithm
 
   def initialize(world)
     @world = world
+    random_schedule
+  end
+
+  private
+
+  def random_schedule
     @world.intersections.each do |(id, intersection)|
-      intersection.in_streets.each do |street|
+      streets = intersection.in_streets
+                            .sort_by { |street| street.cars_count }
+                            .select { |street| street.cars_count.positive? }
+                            .reverse
+
+      streets.each_with_index do |street, i|
         intersection.schedule_entries << ScheduleEntry.new(street_name: street.name, duration: 2)
+      end
+
+      if intersection.schedule_entries.empty?
+        intersection.schedule_entries << ScheduleEntry.new(street_name: intersection.in_streets.first.name, duration: 1)
       end
     end
   end
