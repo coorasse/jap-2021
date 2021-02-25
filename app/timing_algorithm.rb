f62b7a8f278fd5b99027486f468fa42673e40544
Pascal Andermatt <p.andermatt@me.com>
2021-02-25 20:55:13 +0100

    up

--- app/timing_algorithm.rb
+++ app/timing_algorithm.rb
@@ -1,10 +1,19 @@
 class TimingAlgorithm
   def initialize(world)
-    @world = world
+    puts world.simulation_time
 
-    @world.intersections.each do |id, intersection|
-      intersection.in_streets.each do |street|
-        intersection.schedule_entries << ScheduleEntry.new(street_name: street.name, duration: 1)
+    world.intersections.each do |(id, intersection)|
+      # puts '-- Intersection --'
+      total = intersection.in_streets.sum { |s| s.cars_count }
+      if total == 0
+        intersection.schedule_entries << ScheduleEntry.new(street_name: intersection.in_streets.first.name, duration: 1)
+      end
+      intersection.in_streets.sort_by(&:cars_count).reverse.each do |street|
+        if street.cars_count != 0
+          n = ((total * 1.0 / street.cars_count) / 3).to_f.ceil
+          d = n == 0 ? 1 : n
+          intersection.schedule_entries << ScheduleEntry.new(street_name: street.name, duration: d)
+        end
       end
     end
   end
